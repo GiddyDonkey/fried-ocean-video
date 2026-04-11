9ffc464100ec6d177529dd28dbf4b279983bd583
Jason <jasonwolenik@hotmail.com>
2026-04-10 18:59:30 -0700

    force restore working generator

--- deploy.bat
+++ deploy.bat
@@ -4,7 +4,7 @@ copy /Y "%~dp0index.html" "%PROJ%\index.html"
 copy /Y "%~dp0index.html" "%PROJ%\public\index.html"
 cd /d "%PROJ%"
 git add .
-git commit -m "white bg, remove ticker"
+git commit -m "force restore working generator"
 git push
-echo Done! Wait 30 seconds.
+echo Done! Wait 30 seconds then hard refresh the page with Ctrl+Shift+R
 pause
